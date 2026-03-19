5ba88a4c32a0a9b4b2b484dc50004ba14d876f5c
tiagosmarqs <tiagosmarques98@gmail.com>
2026-03-19 14:15:41 -0300

    feat: adiciona menu

--- src/Views/commons/header.php
+++ src/Views/commons/header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
 <head>
+    <base href="<?=URL_BASE?>">
+
+    
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
     <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
@@ -29,5 +32,40 @@
     <link href="<?=URL_BASE?>resources/css/css.css?v=<?=time()?>" rel="stylesheet"/>
 <body>
 	<header>
+    <div class="container">
+      <div class="conteudo">
+        <div class="logo">
+          <img src="<?=URL_BASE?>resources/imagens/logo.png" alt="">
+        </div>
+        <div class="menu-lateral">
+          <ul class="nav">
+            <li>
+              <a href="#quem-sou-eu">Quem sou eu</a>
+            </li>
+            <li>
+              <a href="#eventos">Eventos</a>
+            </li>
+            <li>
+              <a href="#cursos">Cursos</a>
+            </li>
+            <li>
+              <a href="#videos">Videos</a>
+            </li>
+            <li>
+              <a href="#fale-comigo">Fale comigo</a>
+            </li>
+          </ul>
+          <div class="redes-sociais">
+            <p>Siga-me:</p>
+            <a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.youtube.com" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+          </div>
+        </div>
+        <div class="nav-bar">
+          <i class= "fa-solid fa-bars-staggered"></i>
+        </div>
+      </div>
+    </div>
 		
 	</header>
